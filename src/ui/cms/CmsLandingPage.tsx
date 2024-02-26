251431b77de9135c6433ca3c0aa1927c09d30fad
Henrique Yuji Rossetti Inonhe <henriqueinonhe@gmail.com>
2024-02-26 10:55:33 -0300

    Foreign props at UI

--- src/ui/cms/CmsLandingPage.tsx
+++ src/ui/cms/CmsLandingPage.tsx
@@ -1,10 +1,9 @@
 import { LandingPage as CmsLandingPageOwnProps } from "@/infrastructure/cms/schemas";
-import dynamic from "next/dynamic";
 import {
   getCmsComponentRenderer,
   getCmsComponentRendererList,
 } from "./getCmsComponentRenderer";
-import { ContentEnterAnimation } from "../components/ContentAnimationWrapper";
+import dynamic from "next/dynamic";
 
 const LandingPage = dynamic(() =>
   import("../components/LandingPage").then((m) => m.LandingPage),
@@ -15,34 +14,13 @@ export const makeRenderCmsLandingPage =
   () => {
     const renderHero = getCmsComponentRenderer(hero);
 
-    const contentAnimationList = computeContentAnimationList(
-      contentEnterAnimation,
-      content.length,
-    );
     const renderContentList = getCmsComponentRendererList(content);
-    const contentElement = renderContentList.map((render, index) =>
-      render({ enterAnimation: contentAnimationList[index] }),
+
+    return (
+      <LandingPage
+        hero={renderHero()}
+        renderContentList={renderContentList}
+        contentEnterAnimation={contentEnterAnimation}
+      />
     );
-
-    return <LandingPage hero={renderHero()} content={contentElement} />;
   };
-
-const computeContentAnimationList = (
-  contentEnterAnimation: CmsLandingPageOwnProps["contentEnterAnimation"],
-  contentListSize: number,
-) => {
-  const strategies: Record<
-    CmsLandingPageOwnProps["contentEnterAnimation"],
-    () => Array<ContentEnterAnimation>
-  > = {
-    Alternate: () =>
-      Array.from({ length: contentListSize }, (_, index) =>
-        index % 2 === 0 ? "FromLeft" : "FromRight",
-      ),
-    None: () => Array(contentListSize).fill("None"),
-    FromLeft: () => Array(contentListSize).fill("FromLeft"),
-    FromRight: () => Array(contentListSize).fill("FromRight"),
-  };
-
-  return strategies[contentEnterAnimation]();
-};
